4b17b85804e358ccde796b77365a64d6d8fae552
Yuuki Enomoto <uki@e-yuuki.org>
2019-10-10 10:10:01 +0900

    Add argument parser

--- sysutils/netbsd-build/netbsd-build.sh
+++ sysutils/netbsd-build/netbsd-build.sh
@@ -6,6 +6,26 @@ _bomb()
     exit 1
 }
 
+_usage()
+{
+    cat << EOF
+$0 [-r ROOTDIR] [-huU]
+
+ Options
+    -r ROOTDIR      Specify root (parent) directory of src, xsrc,
+                    tools, obj, releasedir, and destdir.
+                    [Default: /zpool]
+    -u              *Do not* run "make cleandir" first.
+                    Without this, everything is rebuilt, including
+                    the tools.
+    -U              Build without requiring root privileges,
+                    install from an UNPRIVED build with proper
+                    file permissions.
+    -h              Show this message.
+EOF
+    exit 0
+}
+
 MACHINE="$(uname -m)"
 
 ROOT="/zpool"
@@ -23,12 +43,25 @@ CMD="./build.sh \
      -T $TOOLS \
      -U \
      -X $XSRC \
-     -m amd64 \
-     -u \
+     -m $MACHINE \
      -x \
      -j3"
 HG="/usr/pkg/bin/hg"
 SUDO="/usr/pkg/bin/sudo"
+
+# Parse arguments
+
+while getopts r:uh OPT; do
+    case $OPT in
+    "r") ROOT="$OPTARG" ;;
+    "u") CMD="$CMD -u" ;;
+    "U") CMD="$CMD -U" ;;
+    "h") _usage ;;
+    *) _usage ;;
+    esac
+done
+
+# Initial check
 
 for dir in $ROOT $SRC $XSRC $OBJ $TOOLS $RELEASE $DEST; do
     test -d "$dir" || _bomb "$dir: not found"
@@ -37,6 +70,8 @@ done
 for command in $SUDO $HG; do
     test -f "$command" || _bomb "$command: command not found"
 done
+
+# Main
 
 cd "$XSRC" \
     && $SUDO $HG pull \
@@ -48,4 +83,5 @@ cd "$SRC" \
     && $SUDO $CMD tools \
     && $SUDO $CMD kernel=GENERIC \
     && $SUDO $CMD modules \
-    && $SUDO $CMD distribution
+    && $SUDO $CMD distribution \
+    && $SUDO $CMD release
