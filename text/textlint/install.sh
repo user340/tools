b46bf413eeadcd7c6f82d767d4910e0bfa5c0b8a
Yuuki Enomoto <uki@e-yuuki.org>
2019-12-28 20:04:33 +0900

    Update textlintrc

--- text/textlint/install.sh
+++ text/textlint/install.sh
@@ -3,28 +3,34 @@
 cmd="npm"
 cmd_opt="install -g"
 targets="textlint
+@textlint-ja/textlint-rule-no-insert-dropping-sa
+@textlint-ja/textlint-rule-no-synonyms
+sudachi-synonyms-dictionary
 textlint-filter-rule-whitelist
-textlint-rule-no-start-duplicated-conjunction
-textlint-rule-no-doubled-conjunction
-textlint-rule-ng-word
-textlint-rule-no-dead-link
-textlint-rule-terminology
-textlint-rule-max-ten
-textlint-rule-no-doubled-joshi
-textlint-rule-no-double-negative-ja
-textlint-rule-ja-no-redundant-expression
-textlint-rule-ja-no-abusage
-textlint-rule-sentence-length
+textlint-plugin-html
 textlint-rule-first-sentence-length
-textlint-rule-no-dropping-the-ra
-textlint-rule-no-doubled-conjunctive-particle-ga
-textlint-rule-no-doubled-conjunction
-textlint-rule-ja-hiragana-keishikimeishi
 textlint-rule-ja-hiragana-fukushi
 textlint-rule-ja-hiragana-hojodoushi
-@textlint-ja/textlint-rule-no-insert-dropping-sa
+textlint-rule-ja-hiragana-keishikimeishi
+textlint-rule-ja-no-abusage
+textlint-rule-ja-no-mixed-period
+textlint-rule-ja-no-redundant-expression
+textlint-rule-ja-no-successive-word
+textlint-rule-ja-no-weak-phrase
+textlint-rule-max-ten
+textlint-rule-ng-word
+textlint-rule-no-dead-link
+textlint-rule-no-double-negative-ja
+textlint-rule-no-doubled-conjunction
+textlint-rule-no-doubled-conjunctive-particle-ga
+textlint-rule-no-doubled-joshi
+textlint-rule-no-dropping-the-ra
+textlint-rule-no-mix-dearu-desumasu
+textlint-rule-no-start-duplicated-conjunction
 textlint-rule-prefer-tari-tari
-textlint-rule-preset-jtf-style"
+textlint-rule-preset-jtf-style
+textlint-rule-sentence-length
+textlint-rule-terminology"
 
 for package in $targets; do
   $cmd "$cmd_opt" "$package"
