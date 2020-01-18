76410f28ddb6b21a56cee4d8dfdc06c6963ace60
Yuuki Enomoto <uki@e-yuuki.org>
2020-01-18 11:30:02 +0900

    it can exec again

--- sysutils/netbsd-build/netbsd-build.sh
+++ sysutils/netbsd-build/netbsd-build.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
-# Copyright (c) 2019 Yuuki Enomoto
+# Copyright (c) 2020 Yuuki Enomoto
 # Copyright (c) 2001-2011 The NetBSD Foundation, Inc.
 # All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 _bomb()
 {
@@ -12,15 +33,9 @@ _bomb()
 _usage()
 {
     cat << EOF
-$0 [-r ROOTDIR] [-huU]
+$0 [-hruU]
 
  Options
-    -a              Set MACHINE_ARCH to arch.
-                    [Default: deduced from MACHINE]
-    -m              Set MACHINE to mach.  Some mach values are
-                    actually aliases that set MACHINE/MACHINE_ARCH
-                    pairs.  [Default: deduced from the host system
-                    if the host OS is NetBSD]
     -r              Remove contents of TOOLDIR and DESTDIR before 
                     building.
     -u              *Do not* run "make cleandir" first.
@@ -31,67 +46,61 @@ $0 [-r ROOTDIR] [-huU]
                     file permissions.
     -h              Show this message.
 EOF
-    exit 0
+    exit 1
 }
 
-ROOT="/zpool"
-SRC="$ROOT/src"
-XSRC="$ROOT/xsrc"
+SRC="$HOME/src/cvs.NetBSD.org/src"
+XSRC="$HOME/src/cvs.NetBSD.org/xsrc"
+
+ROOT="/zshare/netbsd-build/amd64"
 OBJ="$ROOT/obj"
 TOOLS="$ROOT/tools"
 RELEASE="$ROOT/releasedir"
-CMD="./build.sh \
-     -O $OBJ \
-     -R $RELEASE \
-     -T $TOOLS \
-     -V MKDEBUG=yes \
-     -V MKDEBUGLIB=yes \
-     -V MKLINT=yes \
-     -X $XSRC \
-     -x \
-     -j3"
-HG="/usr/pkg/bin/hg"
-SUDO="/usr/pkg/bin/sudo"
+DESTDIR="$ROOT/destdir"
+BUILD_CMD="./build.sh"
+CMD_OPT="-a x86_64 \
+         -m amd64 \
+         -D $DESTDIR \
+         -O $OBJ \
+         -R $RELEASE \
+         -T $TOOLS \
+         -V MKDEBUG=yes \
+         -V MKDEBUGLIB=yes \
+         -V MKLINT=yes \
+         -X $XSRC \
+         -x \
+         -j8"
+PRIV="/usr/local/bin/doas"
+VCS="/usr/pkg/bin/cvs"
+UPDATE_CMD="$VCS update"
+
+for cmd in $PRIV $VCS; do
+    test -x "$cmd" || _bomb "$cmd: command not found or not executable"
+done
+
+for directory in $ROOT $OBJ $TOOLS $RELEASE $DESTDIR $SRC $XSRC; do
+    test -d "$directory" || _bomb "$directory: no such directory"
+done
 
 # Parse arguments
 
 while getopts ruUha:m: OPT; do
     case $OPT in
-    "a") MACHINE_ARCH="$OPTARG" ;;
-    "m") MACHINE="$OPTARG" ;;
-    "r") CMD="$CMD -r" ;;
-    "u") CMD="$CMD -u" ;;
-    "U") CMD="$CMD -U" ;;
+    "r") CMD_OPT="$CMD_OPT -r" ;;
+    "u") CMD_OPT="$CMD_OPT -u" ;;
+    "U") CMD_OPT="$CMD_OPT -U" ;;
     "h") _usage ;;
     *) _usage ;;
     esac
 done
 
-MACHINE=${MACHINE:=$(uname -m)}
-MACHINE_ARCH=${MACHINE_ARCH:=$(uname -p)}
-DEST="$ROOT/destdir/$MACHINE"
-
-CMD="$CMD -a $MACHINE_ARCH -m $MACHINE -D $DEST"
-
-# Initial check
-
-for dir in $ROOT $SRC $XSRC $OBJ $TOOLS $RELEASE $DEST; do
-    test -d "$dir" || _bomb "$dir: not found"
-done
-
-for command in $SUDO $HG; do
-    test -f "$command" || _bomb "$command: command not found"
-done
-
 # Main
 
-cd "$XSRC" \
-    && $SUDO $HG pull \
-    && $SUDO $HG update
+cd "$XSRC" && $UPDATE_CMD
 
 cd "$SRC" \
-    && $SUDO $HG pull \
-    && $SUDO $HG update \
-    && $SUDO $CMD tools \
-    && $SUDO $CMD kernel=GENERIC \
-    && $SUDO $CMD distribution
+    && $UPDATE_CMD \
+    && $PRIV "$BUILD_CMD" $CMD_OPT tools \
+    && $PRIV "$BUILD_CMD" $CMD_OPT kernel=GENERIC \
+    && $PRIV "$BUILD_CMD" $CMD_OPT distribution \
+    && $PRIV "$BUILD_CMD" $CMD_OPT sets
