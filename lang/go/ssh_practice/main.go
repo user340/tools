e113325788da5a6d7be99357dd0f65b3682fc828
Yuuki Enomoto <uki@e-yuuki.org>
2020-04-28 08:06:23 +0900

    Study channel and arrays

--- lang/go/ssh_practice/main.go
+++ lang/go/ssh_practice/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/user340/tools/ssh/ssh"
 )
 
-func main() {
+func doSSHRemoteExec(command string) string {
 	host := "localhost:22"
 	user := "uki"
 	key := "id_ed25519"
@@ -17,6 +17,19 @@ func main() {
 	session := ssh.CreateSessionFor(client)
 	defer session.Close()
 
-	result := ssh.RunCommandOnRemote(session, "uname -a")
-	fmt.Print(result.String())
+	result := ssh.RunCommandOnRemote(session, command)
+
+	return result.String()
+}
+
+func main() {
+	var commands [2]string = [2]string{"uname -a", "ls"}
+	ch := make(chan string, 2)
+
+	for i := 0; i < 2; i++ {
+		go func(command string) {
+			ch <- doSSHRemoteExec(command)
+		}(commands[i])
+		fmt.Println(<-ch)
+	}
 }
