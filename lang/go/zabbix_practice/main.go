64e1b5e84aa115e0d20f7b63676a4adfd9fb6e53
Yuuki Enomoto <uki@e-yuuki.org>
2020-05-01 11:12:51 +0900

    Take Item values from Zabbix using Go

--- lang/go/zabbix_practice/main.go
+++ lang/go/zabbix_practice/main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func startSession(url string, user string, password string) *zabbix.Session {
-	session, err := zabbix.NewSession("http://"+url+"/zabbix/api_jsonrpc.php", user, password)
+	frontend := "http://" + url + "/zabbix/api_jsonrpc.php"
+	session, err := zabbix.NewSession(frontend, user, password)
 	if err != nil {
 		panic(err)
 	}
@@ -23,16 +24,40 @@ func showAPIVersion(session *zabbix.Session) {
 	fmt.Printf("Connected to Zabbix API %v\n", version)
 }
 
-func showAllHosts(session *zabbix.Session) {
+func getAllHostsFrom(session *zabbix.Session) []zabbix.Host {
 	var params zabbix.HostGetParams
-
 	hosts, err := session.GetHosts(params)
 	if err != nil {
 		panic(err)
 	}
 
+	return hosts
+}
+
+func showAllHosts(session *zabbix.Session) {
+	hosts := getAllHostsFrom(session)
+
 	for _, host := range hosts {
 		fmt.Printf("%v\n", host.DisplayName)
+	}
+}
+
+func getItemsFrom(session *zabbix.Session, host string) []zabbix.Item {
+	var params zabbix.ItemGetParams
+	params.Host = host
+	items, err := session.GetItems(params)
+	if err != nil {
+		panic(err)
+	}
+
+	return items
+}
+
+func showItems(session *zabbix.Session, host string) {
+	items := getItemsFrom(session, host)
+
+	for _, item := range items {
+		fmt.Printf("%v -- %v at %v\n", item.ItemName, item.LastValue, item.LastClock)
 	}
 }
 
@@ -41,4 +66,5 @@ func main() {
 
 	showAPIVersion(session)
 	showAllHosts(session)
+	showItems(session, "NetBSD Zabbix")
 }
